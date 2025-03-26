a51314782dc4ed15c75bf1990af077582979814c
Bagsylina <fabian.anghel.2012@gmail.com>
2025-03-26 19:09:12 +0200

    change words into right forms based on the original word's form

--- dex-online-database/dex-online-extraction.py
+++ dex-online-database/dex-online-extraction.py
@@ -18,15 +18,22 @@ words = cursor.fetchall();
 inflection_dict = {}
 
 for word in words:
-    cursor.execute("SELECT inflectionId, formUtf8General FROM inflectedform WHERE lexemeId = {id};".format(id = word["id"]))
+    cursor.execute("SELECT inflectionId, formUtf8General, apheresis FROM inflectedform WHERE lexemeId = {id};".format(id = word["id"]))
     inflections = cursor.fetchall()
     text = word["formUtf8General"]
 
     if len(inflections) > 1:
-        inflection_dict[text] = {"id": word["id"], "word": text, "inflections": []}
+        if text not in inflection_dict:
+            inflection_dict[text] = {"id": word["id"], "word": text, "inflections": {}}
 
-        for inflection in inflections:
-            inflection_dict[text]["inflections"].append({"inflectionId": inflection["inflectionId"], "form": inflection["formUtf8General"]})
+            for inflection in inflections:
+                if inflection["apheresis"] == 0:
+                    inflection_dict[text]["inflections"][inflection["inflectionId"]] = inflection["formUtf8General"]
+        
+        else:
+            for inflection in inflections:
+                if inflection["apheresis"] == 0 and inflection["inflectionId"] not in inflection_dict[text]["inflections"]:
+                    inflection_dict[text]["inflections"][inflection["inflectionId"]] = inflection["formUtf8General"]
 
 with open(join(dirname(__file__), 'word_inflections.json'), 'w') as f:
     json.dump(inflection_dict, f)
